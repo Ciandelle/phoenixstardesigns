797aa363660bfe633146bb61c07275dc6c385f26
Ciandelle Hughes <100923629+Ciandelle@users.noreply.github.com>
2025-03-05 00:16:38 +0000

    first commit

---
+++ _data/site.js
@@ -0,0 +1,19 @@
+module.exports = {
+  meta: {
+    title: "PhoenixStarDesigns",
+    lang: "en",
+    siteUrl: "https://example.com/",
+  },
+  feed: { // used in feed.xml.njk
+    subtitle: "Lorem ipsum dolor sit amet consecuteor",
+    filename: "atom.xml",
+    path: "/atom.xml",
+    id: "https://example.com/",
+    authorName: "John Doe",
+    authorEmail: "johndoe@example.com"
+  },
+  hero: { // used in hero section of main page ie. index.html.njk
+    title: "Hi, my name is Ciandelle Hughes",
+    description: "I am an interaction designer specialising in trauma infomed design"
+  }
+}
